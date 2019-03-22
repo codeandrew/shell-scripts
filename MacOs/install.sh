4423b0d868795a993a9c30e756dcd9a0da1f11b2
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-03-22 17:39:27 +0800

    Update MacOs install script

--- MacOs/install.sh
+++ MacOs/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "Last Working Date: "
+echo "2019-03-22"
 
 echo "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -6,14 +8,31 @@ echo "Installing Homebrew"
 echo "Installing Bash Completion"
 brew install bash-completion
 
+echo
+echo
+echo "Installing Gitlan Runner"
+sudo curl --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-darwin-amd64
+sudo chmod +x /usr/local/bin/gitlab-runner
+
+echo
+echo
 echo "Installing Docker"
 brew cask install docker
 
+echo
+echo
 echo "Installing Virtual Box"
 brew cask install virtualbox
+echo
+echo
+echo "Allow Oracle Inc in Privacy and Security Policy"
 
+echo
+echo
 echo "Installing kubernetes"
 brew install kubectl
 
+echo
+echo
 echo "Installing Minikube"
 brew cask install minikube
