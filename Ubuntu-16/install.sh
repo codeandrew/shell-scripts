c1d7d17aedc08013ce162ffa59fe8c0d8aba134e
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-04-03 14:49:15 +0800

    UB16 Update

--- Ubuntu-16/install.sh
+++ Ubuntu-16/install.sh
@@ -10,6 +10,8 @@ echo '[*] Done Installing Git'
 echo "installing oh my zshell"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+sudo apt-get install tmux
+
 ## For SSH Key
 echo '[!!]'
 echo 'Dont Forget to create ssh key'
--- Ubuntu-16/readme.md
+++ Ubuntu-16/readme.md
@@ -1,14 +1,15 @@
-# Ubuntu Install Shell Script 
+# Ubuntu Install Shell Script
 
-Run this 
-``` 
+Run this
+```
 chmod +x install.sh
 sudo ./install.sh
 ```
 
-## Installs 
+## Installs
 - Git
+- zshell
+- tmux
 - Nginx
 - Mysql 5.7
 - Php 7.2
-
