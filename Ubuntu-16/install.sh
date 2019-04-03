64abe0a07242ee965322a0eaac662cb032ba3aeb
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-04-03 14:01:57 +0800

    refactor shell-scripts

--- Ubuntu-16/install.sh
+++ Ubuntu-16/install.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
-
-#Update fresh Vagrant 
 apt-get update
 apt-get upgrade
 
-# Git 
+# Git
 apt-get install git
 echo '[!!]'
 echo '[*] Done Installing Git'
 
-# Mysql 5.7
-apt-get install mysql-server
-systemctl status mysql
-mysql_secure_installation
-echo '[!!]'
-echo '[*] Done Installing MySQL'
-
-# NginX
-apt-get install nginx
-echo '[!!]'
-echo '[*] Done Installing Nginx'
-
-## PHP 7.2 for ubuntu16
-sudo apt-get install -y software-properties-common
-add-apt-repository ppa:ondrej/php
-apt-get update
-apt-get install php7.2 php7.2-fpm php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
-
-echo '[!!]'
-echo '[*] Done Installing Php 7.2'
+echo "installing oh my zshell"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 ## For SSH Key
 echo '[!!]'
