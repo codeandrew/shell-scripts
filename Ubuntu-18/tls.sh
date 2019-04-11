25394a415128dfeab3d078816ec5bade9d330b83
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-04-11 13:34:20 +0800

    tls

--- Ubuntu-18/tls.sh
+++ Ubuntu-18/tls.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#DOMAIN="ghost.whitecloak.io"
-DOMAIN=""
+DOMAIN="ghost.whitecloak.io"
+# DOMAIN=""
 
 openssl req -x509 -newkey rsa:4096 -sha256 -nodes -keyout tls.key -out tls.crt -subj\
 "/CN=$DOMAIN" -days 365
 
 
-# kubectl create secret tls ghost-whitecloak-com-tls --cert=tls.crt --key=tls.key
-kubectl create secret tls "$DOMAIN"-tls --cert=tls.crt --key=tls.key
+kubectl create secret tls ghost-whitecloak-com-tls --cert=tls.crt --key=tls.key
+# kubectl create secret tls "$DOMAIN"-tls --cert=tls.crt --key=tls.key
