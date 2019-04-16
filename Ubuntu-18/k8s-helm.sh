3cf6d1c76988c30d203304f662404aff1eddfe21
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-04-16 18:52:45 +0800

    Create k8s-helm script

---
+++ Ubuntu-18/k8s-helm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+
+
+echo "Creating tiller"
+helm init --service-account tiller
