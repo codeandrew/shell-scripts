11b27a36bb0983c25001fb373fe9c8aac579bdc5
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-05-23 11:19:15 +0800

    Create k8s using doctl script

---
+++ MacOs/doctl_create_k8s.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "Creating Kubernetest Cluster"
+echo "What's your cluster name"
+read CLUSTER_NAME
+doctl k8s cluster create $CLUSTER_NAME
+kubectl config get-contexts
+
+echo "Do you already have helm? y/n"
+read HELM
+if [ HELM == 'n']
+then
+  cd /tmp
+  curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > install-helm.sh
+  chmod u+x install-helm.sh
+  ./install-helm.sh
+fi
+
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
+kubectl get pods --namespace kube-system
+helm install stable/kubernetes-dashboard --name dashboard-demo
+helm list
+kubectl get services
+
+
