f7f747c00e5283dfd28bc7d7670af4dc9b8b338d
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-06-11 02:20:25 +0000

    Update Helm add tiller account config

--- Ubuntu-18/k8s-helm.sh
+++ Ubuntu-18/k8s-helm.sh
@@ -5,6 +5,14 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 
 
+kubectl --namespace kube-system create serviceaccount tiller
+
+kubectl create clusterrolebinding tiller-cluster-rule \
+ --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+
+kubectl --namespace kube-system patch deploy tiller-deploy \
+ -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
 echo "Creating tiller"
 #helm init --service-account tiller
 helm init
