1b8b81dd7e9c6cf5f614c12dee4c975ee0d9ddd9
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-08-07 15:18:03 +0800

    Add docker-compose in devops script

--- Ubuntu-18/devops.sh
+++ Ubuntu-18/devops.sh
@@ -18,6 +18,13 @@ sudo usermod -a -G docker $USER
 sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 sudo chmod g+rwx "/home/$USER/.docker" -R
 
+
+## Installing Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+
 echo
 echo
 
