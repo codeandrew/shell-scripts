bf4c5cfeca9f927ff61235c009facb9d8c59e967
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2019-12-17 17:13:12 +0800

    Organize install.sh

--- MacOs/install.sh
+++ MacOs/install.sh
@@ -3,8 +3,30 @@ echo "Last Working Date: "
 echo "2019-03-22"
 echo "2019-07-11"
 
+EMAIL="jeanandrewfuentes@gmail.com"
+
 echo "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Generating ssh keygen"
+#ssh-keygen -t rsa
+ssh-keygen -t rsa -b 4096 -C $EMAIL
+echo
+echo
+
+echo "Installing Oh My Z Shell"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo
+echo
+
+Echo "Installing mdless"
+gem install mdless
+echo
+echo
+
+##############################################################################
+## BREW INSTALLS
+##############################################################################
 
 echo "Installing Python"
 brew install python
@@ -12,25 +34,17 @@ echo "Installing Vim"
 brew install vim
 echo "Installing  Tree"
 brew install tree
-
-echo "Generating ssh keygen"
-ssh-keygen -t rsa
-
 echo "Installing Bash Completion"
 brew install bash-completion
-
-echo "Installing Oh My Z Shell"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 
 echo "Installing tmux"
 brew install tmux
 
 cp ../Configs/tmux.conf $HOME/.tmux.conf
 tmux source-file $HOME/.tmux.conf
+echo
+echo
 
-echo
-echo
 echo "Installing Gitlab Runner"
 sudo curl --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-darwin-amd64
 sudo chmod +x /usr/local/bin/gitlab-runner
@@ -68,10 +82,7 @@ echo "Installing Vtop"
 npm install -g vtop
 
 
-echo
-echo
-Echo "Installing mdless"
-gem install mdless
+
 
 
 echo
