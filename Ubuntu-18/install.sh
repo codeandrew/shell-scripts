0ad5f714c443fed98eee19449be6748dabe9c018
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-01-13 16:51:45 +0800

    Organize Ubuntu install script

--- Ubuntu-18/install.sh
+++ Ubuntu-18/install.sh
@@ -1,31 +1,73 @@
-#!/bin/sh
+#!/bin/bash
+
 sudo apt update
-sudo apt install git
 
-git --version
+## PACKAGES
+packages=(
+  git
+  zsh
+  tmux
+  vim
+  traceroute
+  net-tools
+  xclip
+  python3-pip
+  nmap
+  zenmap
+  jq 
+  yq  
+  neofetch
+)
 
-sudo apt-get update && sudo apt-get -y install zsh
-zsh --version
+desktop_packages=(
+  vlc
+  vlc-bin 
+)
 
+for package in "${packages[@]}"
+do
+  echo "---------------------------------"
+  echo "Installing $package"
+  echo
+  sudo apt -y install $package
+  echo
+  echo "---------------------------------"
+  echo
+  echo
+done
+
+sleep 2
+
+## CONFIGS
+
+echo "Copying Configs ..."
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
-chsh -s $(which zsh)
-
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-source ~/.zshrc
-
-sudo apt-get install tmux
+cp ../Configs/profile $HOME/.profile
 cp ../Configs/tmux.conf $HOME/.tmux.conf
+echo
+echo
 
-# Install pip
-sudo apt install python3-pip
+sleep 2
+
 # Set Default python to python3
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 10
-sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
+#sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 10
+#sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
 
 ## For SSH Key
 echo '[!!]'
 echo 'Dont Forget to create ssh key'
 echo 'ssh-keygen -t rsa -b 4096 -C "your_email@gmail.com"'
+
+sudo chsh -s $(which zsh)
+source ~/.zshrc
+
+## ALIASES
+echo "Exporting Aliases"
+echo "alias pbcopy='xclip -selection clipboard'" >> $HOME/.profile
+
+echo
+echo
+
 
 sudo update-alternatives --config editor
