7d353b8aff22e62a7b2408965fa391acf0d379f5
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-01-13 09:14:38 +0800

    Update Ubuntu 18 install script

--- Ubuntu-18/install.sh
+++ Ubuntu-18/install.sh
@@ -9,12 +9,13 @@ zsh --version
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-chsh -s /bin/zsh
+chsh -s $(which zsh)
 
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 source ~/.zshrc
 
 sudo apt-get install tmux
+cp ../Configs/tmux.conf $HOME/.tmux.conf
 
 # Install pip
 sudo apt install python3-pip
@@ -26,3 +27,5 @@ sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
 echo '[!!]'
 echo 'Dont Forget to create ssh key'
 echo 'ssh-keygen -t rsa -b 4096 -C "your_email@gmail.com"'
+
+sudo update-alternatives --config editor
