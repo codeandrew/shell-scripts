0b0b9501eff0b9ff94ccbc17ec1716e972d839c0
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2020-07-05 11:04:43 +0800

    Update kali linux installation process

--- Kali-2019.3/install.sh
+++ Kali-2019.3/install.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 echo $(cat ./sources.list) >> /etc/apt/sources.list
 
+read -p "Make sure to Disable the Screen Lock, as it \
+  corrupts the installation process"
+
+apt-get clean
 apt-get update -y
-apt-get full-upgrade -y
+apt-get upgrade -y
+apt-get dist-upgrade -y
 
 packages=(
   beef-xss
