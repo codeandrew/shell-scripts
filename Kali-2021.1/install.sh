ce3703d6d53f84776a0e669771acf5ffa1027826
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2021-03-15 09:46:48 -0400

    Create install script for Kali 2021.1

---
+++ Kali-2021.1/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+sudo apt-get clean
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+packages=(
+  beef-xss
+  build-essential
+  openssl
+  unzip
+  libssl-dev
+  tmux
+  xclip
+  fzf
+  netdiscover
+  ruby
+  jq 
+  yq  
+  neofetch
+)
+
+for package in "${packages[@]}"
+do
+  echo "---------------------------------"
+  echo "Installing $package"
+  echo
+  sudo apt-get -y install $package
+  echo
+  echo "---------------------------------"
+  echo
+  echo
+done
+
+
+cat ../Configs/ubuntu_profile >> $HOME/.myprofile
+cp ../Configs/tmux.conf $HOME/.tmux.conf
+echo
+echo
+
+echo "source $HOME/.myprofile" >> $HOME/.zshrc
