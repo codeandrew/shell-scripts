a1d02e2a1b881a9ff1cd4df2634813d9d9adb389
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2022-02-21 16:05:29 +0800

    NodeJS config

--- MacOs/nodejs.sh
+++ MacOs/nodejs.sh
@@ -3,6 +3,10 @@
 echo "Downloading Node Version Manager"
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
+
 echo "Installing latest Node"
 nvm install node
 
