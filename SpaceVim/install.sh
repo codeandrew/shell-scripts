5aaa8109c6ea08be9e5f501e5bbfff9a137065ce
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-05-15 17:17:58 +0800

    Update install.sh

--- SpaceVim/install.sh
+++ SpaceVim/install.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 curl -sLf https://spacevim.org/install.sh | bash
+mkdir -p ~/.SpaceVim/autoload/
+mkdir -p  ~/.SpaceVim.d/
 
 cp init.toml ~/.SpaceVim.d/init.toml
-
 cp ./autoload/myspacevim.vim $HOME/.SpaceVim/autoload/
-
-
-
