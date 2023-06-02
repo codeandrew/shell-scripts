faba28bfa2c8d248f4a7e8ff824156d9bc42fdd6
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-06-02 23:56:02 +0800

    Update install-devops-tools.sh

--- Ubuntu-20/install-devops-tools.sh
+++ Ubuntu-20/install-devops-tools.sh
@@ -7,6 +7,13 @@ sudo usermod -aG docker $USER
 $(echo $SHELL)
 docker run hello-world
 
+echo "[+] Installing Kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
 sudo apt install -y \
     python3-pip \
     kubectl \
