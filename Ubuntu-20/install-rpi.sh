7012cafe85e282fb7cc700893fa0a4739ef97028
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-07-12 14:40:44 +0800

    Update install-rpi.sh

--- Ubuntu-20/install-rpi.sh
+++ Ubuntu-20/install-rpi.sh
@@ -30,3 +30,4 @@ sudo reboot
 # https://linuxhint.com/install-ubuntu-server-raspberry-pi/
 # https://ubuntu.com/core/docs/install-raspberry-pi
 # https://gist.github.com/zoilomora/f862f76335f5f53644a1b8e55fe98320
+# https://sergejskozlovics.medium.com/how-to-set-up-a-wireless-ubuntu-server-on-raspberry-pi-89b84dca34d2
