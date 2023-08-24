3386d668f66cb293e47d4670ef2074f4151baa21
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-08-24 21:58:16 +0800

    Update check-network-connections.sh

--- Ubuntu-20/maintenance/check-network-connections.sh
+++ Ubuntu-20/maintenance/check-network-connections.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Ensure the script is run with root privileges
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
 ifconfig | tee ifconfig.txt
 sudo netstat -tuln | tee netstat.txt
 sudo ss -tulnp | tee ss.txt 
