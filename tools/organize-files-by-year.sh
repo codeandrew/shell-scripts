f3e1fc6860410732de7310323abc87f7cc97877b
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2024-01-04 13:42:38 +0800

    Update organize-files-by-year.sh

--- tools/organize-files-by-year.sh
+++ tools/organize-files-by-year.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 files=$(find . -maxdepth 1 -type f)
+IFS=$'\n' # Set Internal Field Separator to newline to handle spaces in filenames
 
 for file in $files; do
-    # Extract the creation year of the file
-    creation_year=$(stat -c %Y "$file" | date +'%Y' -f -)
+    # Extract the creation year of the file 
+    creation_year=$(stat -f "%Sm" -t "%Y" "$file") # for mac
+    # creation_year=$(stat -c %Y "$file" | date +'%Y' -f -) # for linux
 
     mkdir -p "$creation_year"
-    mv "$file" "$creation_year/"
+    mv "$file" "$creation_year/$(basename "$file")"
 done
