5a1b2675a331a2c67cc0c0f19a797358617a0248
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2024-03-15 18:50:58 +0800

    kali rpi5

---
+++ kali-2024.1-rpi5/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+packages=(
+  gobuster
+  beef-xss
+  build-essential
+  openssl
+  unzip
+  libssl-dev
+  tmux
+  xclip
+  fzf
+  netdiscover
+  ruby
+  jq 
+  yq  
+  neofetch
+)
+
+for package in "${packages[@]}"
+do
+  echo "---------------------------------"
+  echo "Installing $package"
+  echo
+  sudo apt-get -y install $package
+  echo
+  echo "---------------------------------"
+  echo
+  echo
+done
+
+
+cat ../Configs/ubuntu_profile >> $HOME/.myprofile
+cp ../Configs/tmux.conf $HOME/.tmux.conf
+echo
+echo
+
+echo "source $HOME/.myprofile" >> $HOME/.zshrc
---
+++ kali-2024.1-rpi5/readme.md
@@ -0,0 +1,26 @@
+#
+
+
+
+## Mount USB Devices
+
+```bash
+┌──(kali㉿wrench-rpi5)-[/media]
+└─$ lsblk  # check if there's mount point
+NAME        MAJ:MIN RM   SIZE RO TYPE MOUNTPOINTS
+sda           8:0    1 115.5G  0 disk
+└─sda1        8:1    1 115.4G  0 part
+mmcblk0     179:0    0  59.5G  0 disk
+├─mmcblk0p1 179:1    0   255M  0 part /boot
+└─mmcblk0p2 179:2    0  59.2G  0 part /
+
+┌──(kali㉿wrench-rpi5)-[/media]
+└─$ sudo mkdir /mnt/usbdrive
+
+┌──(kali㉿wrench-rpi5)-[/media]
+└─$ sudo mount /dev/sda1 /mnt/usbdrive
+
+┌──(kali㉿wrench-rpi5)-[/media]
+└─$ cd /mnt/usbdrive
+
+```
